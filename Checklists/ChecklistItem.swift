e6b95074baa9560954b945ed625a1da891b903c9
Julius Bzozowski <jbzozowski@gmail.com>
2018-03-09 13:48:59 -0500

    Initial commit

---
+++ Checklists/ChecklistItem.swift
@@ -0,0 +1,18 @@
+//
+//  ChecklistItem.swift
+//  Checklists
+//
+//  Created by Julius Bzozowski on 3/9/18.
+//  Copyright © 2018 Julius Bzozowski. All rights reserved.
+//
+
+import Foundation
+
+class ChecklistItem {
+    var text = ""
+    var checked = false
+    
+    func toggleChecked() {
+        checked = !checked
+    }
+}
